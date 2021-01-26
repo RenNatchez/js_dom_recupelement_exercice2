7dd3b00775e2a938d1d71f6f87e8470db6c4eb7f
natchez <natchezzz@hotmail.com>
2021-01-26 14:03:19 +0100

    fin

---
+++ main.js
@@ -0,0 +1,16 @@
+let a = document.body.innerHTML
+// console.log(a)
+// console.log(document.querySelector('h1').textContent)
+var elementfirst = document.body.querySelector('h1').textContent
+
+console.log(elementfirst)
+var elementLast = document.body.lastElementChild
+console.log(elementLast)
+var ex04 =  document.body.querySelector('div').textContent
+var ex04 =  document.body.querySelector('div').textContent
+console.log(ex04)
+var elementfirstli = document.getElementsByTagName('li')[0]
+var element2 = elementfirstli.nextElementSibling
+console.log(element2)
+
+console.log(element2.previousElementSibling)
